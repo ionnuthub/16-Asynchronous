14ab4cc0da6844cc6e13e7aa55d9ca5b7d8ebd49
ionnuthub <raducugionut@gmail.com>
2023-09-01 15:48:42 +0200

    XML Ajax call

--- starter/script.js
+++ starter/script.js
@@ -4,3 +4,39 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText); // this is the response from request
+
+    //We convert the response JSON data in to an object
+    const [data] = JSON.parse(this.responseText);
+    // We use destructuring to get the obj out of the array
+    console.log(data);
+
+    //Building card component
+
+    const html = ` 
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('romania');
+getCountryData('germany');
+getCountryData('france');
